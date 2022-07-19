c661c7db54f73c91682f00ef45fa7efaa1277b15
ciel <ciel-yu@users.noreply.github.com>
2022-07-19 09:28:04 +0800

    add os checking

--- plugins/ciel/ciel.plugin.zsh
+++ plugins/ciel/ciel.plugin.zsh
@@ -1,3 +1,5 @@
-alias gls='gls -F --color=auto --time-style=long-iso'
-alias ls='ls -GF'
-unalias gm
+if [[ `uname` == "Darwin" ]]; then
+    alias gls='gls -F --color=auto --time-style=long-iso'
+    alias ls='ls -GF'
+    unalias gm
+fi
